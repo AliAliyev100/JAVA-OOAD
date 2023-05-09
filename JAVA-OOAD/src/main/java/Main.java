e6d12b986c39e2ba0f5759b3f4eba4950cb5a329
Your Name <you@example.com>
2023-05-09 20:31:11 +0400

    Done

--- JAVA-OOAD/src/main/java/Main.java
+++ JAVA-OOAD/src/main/java/Main.java
@@ -3,7 +3,6 @@ import org.example.People.Admin;
 import org.example.People.User;
 import org.example.Product.*;
 
-import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
@@ -11,8 +10,9 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         User currentuser = null;
-
+        ProductList.products.size(); // Calls the products to load everything directly
         while(true) {
+            // Checking user's initial actions - register / login / exit'
             currentuser = null;
             System.out.println("Press 1 to register\nPress 2 to login\nPress -1 to exit");
             int logReg = sc.nextInt();
@@ -36,7 +36,7 @@ public class Main {
             }
 
             while (true) {
-
+                // Checking user's actions and doing certain things depending on user input
                 System.out.println("Press 1 to Add product to Cart\nPress 2 to Delete product From cart\nPress 3 to Edit Cart\nPress 4 to view Cart\nPress 5 to get Cart Total Price");
                 if (currentuser.isIfAdmin()) {
                     System.out.println("Press 6 to Create new product\nPress 7 to Delete Product from Product List");
@@ -75,14 +75,14 @@ public class Main {
                     String productDescription = sc.nextLine();
 
                     System.out.println("Type price per unit of Product");
-                    float pricePerUnitOfProduduct = sc.nextFloat();
+                    float pricePerUnitOfProduct = sc.nextFloat();
                     System.out.println("Type expiry year, month and date pf product");
                     int expiryYear = sc.nextInt();
                     int expiryMonth = sc.nextInt();
-                    int expirtyDate = sc.nextInt();
+                    int expiryDate = sc.nextInt();
                     System.out.println("Choose product Type: 1 for Alcoholic Beverage. 2 for Bakery, 3 for Dairy, 4 for Frozen, 5 for Fruit, 6 for Hygiene, 7 for Meat, 8 for Soft Drink, 9 for Sweet and 10 for Tobacco");
                     int productType = sc.nextInt();
-                    addedProduct = ((Admin) currentuser).createProduct(productType, productName, productDescription, pricePerUnitOfProduduct, expiryYear, expiryMonth, expirtyDate);
+                    addedProduct = ((Admin) currentuser).createProduct(productType, productName, productDescription, pricePerUnitOfProduct, expiryYear, expiryMonth, expiryDate);
                     ((Admin) currentuser).addProduct(addedProduct);
                 } else if(userAction == 7){
                     System.out.println("Choose product ID to delete:");
